73b661ce5c2038977a22c7e88b2065732d7edc93
merman-bv <a2bv@outlook.com>
2024-02-29 13:23:46 +0400

    Second argv

--- notes-cw.py
+++ notes-cw.py
@@ -1,1 +1,9 @@
-# ddff
+#-*- coding: UTF-8 -*-
+from sys import argv
+
+script, first, second, third = argv
+
+print ("Этот скрипт называется: ", script)
+print ("Значение первой переменной: ", first)
+print ("Значение второй переменной: ", second)
+print ("Значение третьей переменной: ", third)
