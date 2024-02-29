1d7ee36012bac61a92092fb8bdacb5ddadc5ad59
merman-bv <a2bv@outlook.com>
2024-02-29 14:28:13 +0400

    Кол. аргуметов

--- notes-cw.py
+++ notes-cw.py
@@ -6,6 +6,7 @@ if len(argv)==2:
     script, command = argv
     print("Этот скрипт называется: ", script)
     print("Значение первой переменной: ", command)
+    detectionCommand(command)
 else:
     script = argv
     print("Этот скрипт называется: ", script)
@@ -13,6 +14,14 @@ else:
 with open('notes.json', 'r') as notes_file:
     notes_data = json.load(notes_file)
     print(notes_data)
+
+def detectionCommand(command): # Определение действия
+    if command=="show":
+        print("detection", "show")
+    elif command=="add":
+        print("detection", "add")
+
+
 
 # script = argv
 # script, first, second, third = argv
