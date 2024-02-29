2143f567e72bcb61550fa0e428b182a5fa5831e7
merman-bv <a2bv@outlook.com>
2024-02-29 14:07:29 +0400

    Кол. аргуметов

--- notes-cw.py
+++ notes-cw.py
@@ -1,9 +1,23 @@
 #-*- coding: UTF-8 -*-
 from sys import argv
+import json
 
-script, first, second, third = argv
+if len(argv)==2:
+    script, command = argv
+    print("Этот скрипт называется: ", script)
+    print("Значение первой переменной: ", command)
+else:
+    script = argv
+    print("Этот скрипт называется: ", script)
 
-print ("Этот скрипт называется: ", script)
-print ("Значение первой переменной: ", first)
-print ("Значение второй переменной: ", second)
-print ("Значение третьей переменной: ", third)
+with open('notes.json', 'r') as notes_file:
+    notes_data = json.load(notes_file)
+    print(notes_data)
+
+# script = argv
+# script, first, second, third = argv
+
+# print("Этот скрипт называется: ", script)
+# print("Значение первой переменной: ", first)
+# print("Значение второй переменной: ", second)
+# print("Значение третьей переменной: ", third)
